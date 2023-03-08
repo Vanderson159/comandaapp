5f60f8f3f0e509faa3a51b7263e0fb51ad105ec9
Vanderson Duarte Maciel <78940842+Vanderson159@users.noreply.github.com>
2023-03-08 08:12:33 -0300

    link novo do host

--- lib/data/base_url.dart
+++ lib/data/base_url.dart
@@ -1,4 +1,4 @@
-const baseHost = 'https://38e5-187-5-248-132.sa.ngrok.io/';
+const baseHost = 'https://9d8a-187-5-248-132.sa.ngrok.io/';
 const baseUrl = '${baseHost}comandaWEBapi/api';
 const baseUrlLogin = '${baseHost}comandaWEBapi/api/login';
 const baseUrlRegister = '${baseHost}comandaWEBapi/api/register';
